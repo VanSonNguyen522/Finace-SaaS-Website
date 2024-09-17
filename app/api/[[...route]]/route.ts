3873175d3f67705f7d47c236359ae28731116aa3
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2024-09-17 11:54:13 +0700

    get post api in honojs and middleware for protecting api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,42 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
 
-export const runtime = 'edge'
+export const runtime = 'edge';
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/api');
 
-app.get('/hello', (c) => {
-  return c.json({
-    message: 'Hello Next.js!',
+app
+  .get('/hello', (c) => {
+    return c.json({
+      message: 'Hello Next.js!',
+    });
   })
-})
+  .get('/hello/:test', (c) => {
+    const testParam = c.req.param('test');
+    return c.json({
+      message: `Hello, ${testParam}!`,
+    });
+  })
+  .post(
+    '/create/:postId',
+    zValidator(
+      'json',
+      z.object({
+        name: z.string(),
+        userId: z.number(),
+      })),
+      zValidator(
+        'param',
+        z.object({
+          postId: z.number(),
+        })),
+    (c) => {
+        const { name, userId } = c.req.valid("json")
+        const { postId } = c.req.valid('param')
+        return c.json({});
+    });
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
