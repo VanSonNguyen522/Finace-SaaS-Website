1a58c62e38279af1e0f90cc2331ab039b2188fb6
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2024-09-17 10:08:37 +0700

    setup honojs

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,9 @@
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <p>
-      This is a an authenticated route 
-    </p>
+
+      <UserButton afterSignOutUrl="/"/>
   )
 }
+
