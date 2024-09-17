36a9868238ae63d6b1caa06e85c76433e2776b15
VanSonNguyen522 <167949951+VanSonNguyen522@users.noreply.github.com>
2024-09-17 20:29:45 +0700

    update middleware and protect api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,41 +2,28 @@ import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
 import { zValidator } from '@hono/zod-validator';
 import { z } from 'zod';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import { error } from 'console';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
 app
-  .get('/hello', (c) => {
-    return c.json({
-      message: 'Hello Next.js!',
-    });
-  })
-  .get('/hello/:test', (c) => {
-    const testParam = c.req.param('test');
-    return c.json({
-      message: `Hello, ${testParam}!`,
-    });
-  })
-  .post(
-    '/create/:postId',
-    zValidator(
-      'json',
-      z.object({
-        name: z.string(),
-        userId: z.number(),
-      })),
-      zValidator(
-        'param',
-        z.object({
-          postId: z.number(),
-        })),
+  .get(
+    '/hello',
+    clerkMiddleware(),
     (c) => {
-        const { name, userId } = c.req.valid("json")
-        const { postId } = c.req.valid('param')
-        return c.json({});
-    });
+      const auth =getAuth(c);
+      if(!auth?.userId){
+        return c.json({ error: "Unauthorize"});
+      }
+      return c.json({
+        message: 'Hello Next.js!',
+        userId: auth.userId,
+      });
+  });
+  
 
 export const GET = handle(app);
 export const POST = handle(app);
